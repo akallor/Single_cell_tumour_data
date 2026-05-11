009f250c1f1c024904701d6b255f8951b02a4448
akallor <ashwinkallor@gmail.com>
2026-05-11 23:29:35 +0200

    Add TODO for downloading Sus scrofa reference
    Added a TODO comment to download the Sus scrofa reference and GTF annotation.

--- data_download.sh
+++ data_download.sh
@@ -6,3 +6,4 @@ fasterq-dump --split-files SRS18169578
 fasterq-dump --split-files SRS18169579
 fasterq-dump --split-files SRS18169580
 pigz *.fastq   # compress in parallel
+#TODO: Download the Sus scrofa reference: Ensembl Sscrofa11.1 (GCA_000003025.6), including the GTF annotation file.
